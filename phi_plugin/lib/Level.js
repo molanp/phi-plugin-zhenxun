11f06f86f489918004c11570b2dc04da48d67bc9
molanp <104612722+molanp@users.noreply.github.com>
2025-06-17 21:14:46 +0800

    chore(phi_plugin): 删除未使用的库文件
    - 移除了 phi_plugin/lib 目录下所有未使用的库文件
    - 包括 AES.js、Base64.js、ByteReader.js 等多个文件
    - 这些文件可能在未来的 PhigrosLibrary 版本中会被移除

--- phi_plugin/lib/Level.js
+++
@@ -1,6 +0,0 @@
-const Level = {
-    EZ: 0,
-    HD: 1,
-    IN: 2,
-    AT: 3,
-};
--- phi_plugin/lib/LevelRecord.js
+++
@@ -1,9 +0,0 @@
-class LevelRecord {
-    constructor() {
-        this.fc = false;
-        this.score = 0;
-        this.acc = 0.0;
-    }
-}
-
-export default LevelRecord
